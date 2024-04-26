887e0fb3849a62a5fabcec33d071cd234fef2b02
Mads Sobering <madisonsobering@gmail.com>
2024-04-25 23:19:00 -0500

    Added pagination to the partial for products, and updated the categories show page to be able to paginate related products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all.includes(:categories)
+    @products = Product.all.page(params[:page])
   end
 
   def show
