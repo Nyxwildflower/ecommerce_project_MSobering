0fa7c7a0522df573915ebcb5da291744ff7a6a8a
Mads Sobering <madisonsobering@gmail.com>
2024-04-26 12:23:04 -0500

    Added the category filter to the search only when a category is selected.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,12 +15,21 @@ class ProductsController < ApplicationController
 
     if @query.strip == ""
       redirect_to "/"
-    else
+    elsif params[:category_filter].blank?
       # Search for keyword in Product names or descriptions.
       @products = Product.where('name LIKE ?', "%#{@query}%").or(Product.where('description LIKE ?', "%#{@query}%")).page(params[:page])
 
       # Count all of the products found.
       @count = @products.total_count
+    else
+      @category_filter = params[:category_filter]
+
+      # Search products with a category filter.
+      @products = Product.where('products.name LIKE ?', "%#{@query}%").or(Product.where('products.description LIKE ?', "%#{@query}%")).includes(:categories).where('categories.name = ?', @category_filter).references(:categories).page(params[:page])
+
+      # Count all of the products found.
+      @count = @products.total_count
     end
+
   end
 end
