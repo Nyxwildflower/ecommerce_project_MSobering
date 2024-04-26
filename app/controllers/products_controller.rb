86c217d18b4d5986d2d6653f5a1955a2a84bc2bb
Mads Sobering <madisonsobering@gmail.com>
2024-04-25 21:33:04 -0500

    Created a show action and view with a non-functional add to cart button. Styling looks bad for mobile.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.includes(:categories)
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
