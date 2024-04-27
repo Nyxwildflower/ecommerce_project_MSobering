2cd2f633b3ffd8571c3669c0fead1778844989b0
Mads Sobering <madisonsobering@gmail.com>
2024-04-27 14:18:42 -0500

    Created two application-wide methods to create a cart in session and find the products that the cart references. The load cart method also calculates the total for the cart.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_cart
+  before_action :load_cart
+
+  private
+
+  def initialize_cart
+    session[:cart] ||= []
+  end
+
+  def load_cart
+    @cart_total = 0
+    @cart = Product.find(session[:cart])
+
+    # Calculate the subtotal with sales prices. Items without sales subtract 0.
+    @cart.each do |add|
+      @cart_total += add.price - add.price * add.sale_percentage
+    end
+  end
 end
