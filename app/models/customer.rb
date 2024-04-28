efde909bf0c59ecb44bfa9985fb7a97db6ba2d4f
Mads Sobering <madisonsobering@gmail.com>
2024-04-28 17:08:24 -0500

    Devise added modules to this file, and hashed_password/password_salt were removed from the presence validation.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,12 @@
 class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   belongs_to :province
   has_many :orders
 
-  validates :name, :email, :address, :username, :hashed_password, :password_salt, presence: true
+  validates :name, :email, :address, :username, presence: true
   validates :name, length: { minimum: 2 }
   validates :username, uniqueness: true, length: { minimum: 7 }
   validates :email, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
