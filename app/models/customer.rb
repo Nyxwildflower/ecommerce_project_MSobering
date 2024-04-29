b69aece708ff2a75e7d086c68567444096dd148c
Mads Sobering <madisonsobering@gmail.com>
2024-04-29 12:11:14 -0500

    Added a validation to only allow letters and numbers in a username.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
   belongs_to :province
   has_many :orders
 
@@ -10,4 +10,5 @@ class Customer < ApplicationRecord
   validates :name, length: { minimum: 2 }
   validates :username, uniqueness: true, length: { minimum: 7 }
   validates :email, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
+  validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 end
