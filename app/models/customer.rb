019b3d8075cc029a7c8d0f6d7ecbe3da8e449a68
Mads Sobering <madisonsobering@gmail.com>
2024-04-30 22:26:28 -0500

    Added username back to the presence validations, minor rearranging.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,14 +1,12 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :validatable
+  devise :database_authenticatable, :registerable, :validatable
   belongs_to :province
   has_many :orders
 
-  validates :name, :email, :address, :username, presence: true
-  validates :name, length: { minimum: 2 }
-  validates :username, uniqueness: true, length: { minimum: 7 }
+  validates :name, :email, :username, :address, presence: true
+  validates :name, :address, length: { minimum: 2 }
+  validates :username, uniqueness: true, length: { minimum: 7 }, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "can only contain letters and numbers" }
   validates :email, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP
-  validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "can only contain letters and numbers" }
 end
