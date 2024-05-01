9e8e9e8e2c429143273025250b84bc28c7c27db1
Mads Sobering <madisonsobering@gmail.com>
2024-04-30 22:22:20 -0500

    Overrode Devise's after_sign_in_path to go back to the last page. Stored tax calculations on the order total in an instance variable.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,21 @@ class ApplicationController < ActionController::Base
   before_action :configure_account_update_params, if: :devise_controller?
   before_action :initialize_cart
   before_action :load_cart
+  before_action :site_location
+
+  def site_location
+    if (request.path != "/customers/sign_in" && !current_customer)
+      session[:previous_url] = request.fullpath
+    end
+  end
 
   protected
+
+  def after_sign_in_path_for(resource)
+    previous_path = session[:previous_url]
+    session[:previous_url] = nil
+    previous_path || public_root_path
+  end
 
   def configure_sign_up_params
     attributes = [:name, :username, :email, :password, :password_confirmation, :address, :province_id]
@@ -38,6 +51,13 @@ class ApplicationController < ActionController::Base
 
       # Add up total price including sales and quantities.
       @cart_total += subtotal
+    end
+
+    if customer_signed_in?
+      province = Province.find(current_customer[:province_id])
+      @taxes = {pst: BigDecimal(@cart_total * province.pst.to_f, 2),
+                gst: BigDecimal(@cart_total * province.gst.to_f, 2),
+                hst: BigDecimal(@cart_total * province.hst.to_f, 2)}
     end
   end
 end
